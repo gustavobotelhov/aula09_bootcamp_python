f90bb94beb8d758381dd53b773abbf25379a0b2c
Gustavo Botelho Vieira <gu-botelho@hotmail.com>
2024-08-25 21:25:01 -0300

    preparando o ambiente e adicionando primeiros arquivos

---
+++ etl.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import os # para interagir com windows (comandos tipo ls)
+import glob 
+
+
+# uma funcao de extract que le e consolida os json
+
+def extrair_dados(pasta: str) -> pd.DataFrame:
+    arquivos_json = glob.glob(os.path.join(pasta, '*.json')) #glob.glob para listar tudo dentro da pasta
+    df_list = [pd.read_json(arquivo) for arquivo in arquivos_json] #loop para ler todos arquivos json e transformar em dataframe
+    df_total = pd.concat(df_list, ignore_index=True) #concatena os dataframes do df_list e ignora o index 
+    # na aws sempre vai concatenar pq separa por dia.   
+    return df_total
+
+# uma funcao que transforma
+# adicionando coluna.
+def calcular_kpi_de_total_de_vendas(df: pd.DataFrame) -> pd.DataFrame:
+    df["Total"] = df["Quantidade"] * df["Venda"]
+    print(df)
+    return df
+
+# uma funcao que da load em csv ou parquet
+
+def carregar_dados(df: pd.DataFrame, format_saida: list): #procedure que só vai salvar, não tem retorno
+    """
+    parametro que vai ser ou "csv" ou "parquet" ou "os dois"
+    """
+    print(format_saida)
+    for formato in format_saida:
+        if formato == 'csv':    
+            df.to_csv("dados.csv")
+        if formato == 'parquet':    
+            df.to_parquet("dados.parquet")
+
+def pipeline_calcular_kpi_de_vendas_consolidado(pasta_argumento: str, formato_de_saida: list): 
+    data_frame = extrair_dados(pasta_argumento)
+    data_frame_calculado = calcular_kpi_de_total_de_vendas(data_frame)
+    carregar_dados(data_frame_calculado,formato_de_saida)
